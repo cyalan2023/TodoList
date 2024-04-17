4e30cf5838621e098ed8747976da619121603fc9
Alex <cyalan2019@gmail.com>
2024-04-17 19:03:06 +0500

    todo

---
+++ src/model/todolists-reducer.ts
@@ -0,0 +1,28 @@
+import { TodolistType } from '../App'
+import { v1 } from 'uuid'
+
+let todolistID1 = v1()
+let todolistID2 = v1()
+
+const initialState: TodolistType[] = [
+    { id: todolistID1, title: 'What to learn', filter: 'all' },
+    { id: todolistID2, title: 'What to buy', filter: 'all' },
+]
+
+export const todolistsReducer = (state = initialState, action: any):TodolistType[] => {
+    switch (action.type) {
+        case 'REMOVE-TODOLIST': {
+          return state.filter(tl => tl.id !== action.payload.id)
+        }
+        case 'ADD-TODOLIST': {
+            const todolistId = v1()
+            const newTodolist: TodolistType = {id: todolistId, title: action.payload.title, filter: 'all'}
+            return [...state,newTodolist]
+        }
+        case 'CHANGE-TODOLIST-TITLE': {
+            return state.map(el => el.id === action.payload.id ? {...el, title: action.payload.title} : el)
+        }
+        default:
+            throw new Error("I don't understand this type")
+    }
+}
